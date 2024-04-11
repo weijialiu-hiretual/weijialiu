9886510209d6ffc57c4b53e822c1461c264c71c3
weijialiu <weijialiu@hireez.com>
2024-04-11 14:56:27 +0800

    add dev-console-improvement.js

---
+++ tampermonkey/dev-console-improvement.js
@@ -0,0 +1,162 @@
+// ==UserScript==
+// @name     Dev Console Improvement
+// @version  1
+// @description  Improve dev console UI
+// @author   Veeja
+// @grant    none
+// @match https://dev-console.prod2.hireez.info/*
+// ==/UserScript==
+
+/**
+ * 修改字段选择下拉菜单样式
+ */
+function modifyElements() {
+    var labels = document.querySelectorAll('.dmc-label');
+    for (var i = 0; i < labels.length; i++) {
+        labels[i].style.fontFamily = 'monospace'; //更改字体为等宽字体
+    }
+
+    var listItems = document.querySelectorAll('.dropdown-content li');
+    for (var i = 0; i < listItems.length; i++) {
+        listItems[i].style.margin = '0px'; //减小外边距
+        listItems[i].style.padding = '0px'; //减小内边距
+    }
+
+    var dropdowns = document.querySelectorAll('.dropdown-content');
+    for (var i = 0; i < dropdowns.length; i++) {
+        var dropdown = dropdowns[i];
+        if (dropdown) {
+            dropdown.style.width = 'auto';
+            dropdown.style.width = '440px';
+            dropdown.style.height = 'auto';
+            dropdown.style.maxHeight = '500px';
+            dropdown.style.overflowY = 'auto'; // 设置纵向滚动
+            dropdown.style.overflowX = 'hidden'; // 禁止横向滚动
+            dropdown.style.flexDirection = 'column'; // 确保元素纵向排列
+            dropdown.style.display = 'block'; // 确保元素纵向排列
+
+        }
+    }
+}
+
+
+/**
+ * 添加一个按钮，用于隐藏侧边栏
+ */
+function addHideSideButton() {
+    var sidebar = document.querySelector('aside');
+    if (sidebar) {
+        // 创建一个按钮并添加到body
+        var toggleButton = document.createElement('button');
+        toggleButton.textContent = '<';
+        toggleButton.style.position = 'fixed';
+        toggleButton.style.zIndex = '9999';
+        toggleButton.style.top = '10px';
+        var rect = sidebar.getBoundingClientRect();
+        toggleButton.style.left = (rect.right) + 'px';
+
+        // 添加一些样式使按钮看起来更漂亮
+        toggleButton.style.width = '35px';
+        toggleButton.style.height = '30px';
+        toggleButton.style.padding = '';
+        toggleButton.style.fontSize = '14px';
+        toggleButton.style.border = 'none';
+        toggleButton.style.borderRadius = '0 50% 50% 0'; // 修改这里
+        toggleButton.style.backgroundColor = '#007BFF';
+        toggleButton.style.color = 'white';
+        toggleButton.style.cursor = 'pointer';
+        toggleButton.style.boxShadow = '0px 8px 15px rgba(0, 0, 0, 0.1)';
+        toggleButton.style.transition = 'all 0.3s ease 0s';
+
+        document.body.appendChild(toggleButton);
+
+        // 为按钮添加点击事件
+        toggleButton.addEventListener('click', function () {
+            // 切换侧边栏的显示状态
+            if (sidebar.style.width !== '0px') {
+                sidebar.style.width = '0px';
+                toggleButton.textContent = '>';
+                toggleButton.style.left = '0px';
+            } else {
+                sidebar.style.width = '';
+                toggleButton.textContent = '<';
+                toggleButton.style.left = (rect.right) + 'px';
+            }
+        });
+
+        // 当侧边栏找到后，我们就不需要再观察了
+        observer.disconnect();
+    }
+}
+
+/**
+ * 修改表格列宽度, 限制表格列的最大宽度
+ */
+function limitTableMaxWidth() {
+    // 选择你需要修改的元素，这里选择的是所有的表格列元素
+    var tableCells = document.querySelectorAll('td');
+    // 遍历所有的表格列元素
+    for (var i = 0; i < tableCells.length; i++) {
+        // 设置最大宽度，你可以根据你的需求修改这个值
+        tableCells[i].style.maxWidth = '600px';
+        // 如果需要，你还可以添加一个横向滚动条
+        tableCells[i].style.overflowX = 'hidden';
+    }
+}
+
+/**
+ * limit sql text height
+ */
+function limitSqlTextHeight() {
+    var sqlTexts = document.querySelectorAll('.text-xs');
+    // 遍历所有的text-xs类的元素
+    for (var j = 0; j < sqlTexts.length; j++) {
+        // 设置最大高度为3行，你可以根据你的需求修改这个值
+        // 这里假设每行的高度为1em，所以3行的高度为3em
+        sqlTexts[j].style.maxHeight = '5em';
+        // 如果需要，你还可以添加一个纵向滚动条
+        sqlTexts[j].style.overflowY = 'auto';
+        // 设置等宽字体
+        sqlTexts[j].style.fontFamily = '"Courier New", monospace';
+    }
+}
+
+
+/**
+ * 添加新的选项，默认选择显示100条
+ */
+function addNewOption() {
+    var selects = document.querySelectorAll('.dmc-select');
+    selects.forEach(function (select) {
+        // 检查是否存在你添加的option
+        var optionExists = select.querySelector('option[value="100"]');
+        // 创建一个新的option元素，设置其值为100
+        if (!optionExists) {
+            var option = document.createElement('option');
+            option.value = '100';
+            option.text = 'Show 100';
+            select.appendChild(option);
+            // 设置select的value为新option的value
+            select.value = '100';
+            // 触发一个change事件
+            var event = new Event('change', {bubbles: true});
+            select.dispatchEvent(event);
+        }
+    });
+}
+
+var observer = new MutationObserver(function (mutations) {
+    modifyElements();
+    addHideSideButton();
+    limitTableMaxWidth();
+    limitSqlTextHeight();
+    addNewOption();
+});
+
+observer.observe(document.body, {
+    childList: true, subtree: true
+});
+
+window.onload = function () {
+    modifyElements();
+}
