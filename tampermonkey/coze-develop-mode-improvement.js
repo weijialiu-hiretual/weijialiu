0cfead3d293560dbe719e0408ad23f9633b16a0d
weijialiu <weijialiu@hireez.com>
2024-04-11 18:46:23 +0800

    add coze-develop-mode-improvement.js

---
+++ tampermonkey/coze-develop-mode-improvement.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name     Coze Style Change
+// @version  1
+// @description
+// @author   Veeja
+// @grant    none
+// @match https://www.coze.com/space/*
+// ==/UserScript==
+
+/**
+ *
+ */
+function modifyElements() {
+    var box = document.querySelectorAll('.qrPNrOyVEBA326VHThBn');
+    // 修改 display 为 flex
+    for (var i = 0; i < box.length; i++) {
+        box[i].style.display = 'flex';
+    }
+
+    var elements = document.querySelectorAll('.GgZHQbhCPZYqXVil_nkc')
+    var persona = elements[0];
+    var skill = elements[1];
+    var chat = elements[2];
+
+    // 将persona和skill的宽度设为50%
+    persona.style.width = '10%';
+    skill.style.width = '10%';
+
+    // 将chat的宽度设为100%
+    chat.style.width = '200%';
+}
+
+
+var observer = new MutationObserver(function (mutations) {
+    modifyElements();
+});
+
+observer.observe(document.body, {
+    childList: true, subtree: true
+});
+
+window.onload = function () {
+    modifyElements();
+}
