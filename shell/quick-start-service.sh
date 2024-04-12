eec61a8a7a147465730cf84ebeca64ba2f36aa00
weijialiu <weijialiu@hireez.com>
2024-04-12 09:18:36 +0800

    quick start services

---
+++ shell/quick-start-service.sh
@@ -0,0 +1,13 @@
+function startBackend() {
+    # 进入目录 ~/WebstormProjects/TM-Hiretual-Backend/
+    # 执行命令 /Users/weijialiu/.nvm/versions/node/v18.12.1/bin/npm start
+    cd ~/WebstormProjects/TM-Hiretual-Backend/
+    /Users/weijialiu/.nvm/versions/node/v18.12.1/bin/npm start
+}
+
+function startNodeScheduler() {
+    # 进入目录 ~/WebstormProjects/nodescheduler-v2
+    # 执行命令 /Users/weijialiu/.nvm/versions/node/v18.12.1/bin/npm start
+    cd ~/WebstormProjects/nodescheduler-v2
+    /Users/weijialiu/.nvm/versions/node/v18.12.1/bin/npm start
+}
